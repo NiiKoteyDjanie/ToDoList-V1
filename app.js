682e94dbcf22d9191b7f28469ab07b980f91a6fd
NiiKoteyDjanie <sampson.kotey.djanie@gmail.com>
2022-10-08 04:22:57 +0000

    use templates and layout in ejs

--- app.js
+++ app.js
@@ -4,11 +4,17 @@ const bodyParser = require('body-parser');
 const app = express();
 
 let items = ["Code", "Get Paid", "Enjoy Life"];
+let workItems = [];
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+
 app.get('/', function (req, res) {
+  
   let today = new Date();
+  
   let options = {
     weekday: "long",
     day: "numeric",
@@ -16,15 +22,32 @@ app.get('/', function (req, res) {
 };
 
   let day = today.toLocaleDateString("en-US", options);
-  res.render('list', { kindOfDay: day, newListItems: items });
+  res.render('list', { listTitle: day, newListItems: items });
 });
 
 app.post("/", function(req, res){
+  console.log(req.body);
   let item = req.body.newItem;
-  items.push(item);
-  res.redirect("/");
+
+  if (req.body.list === "Work List"){
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");}
+
+});
+
+app.get("/work", function(req, res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+
+});
+
+app.get("/about", function(req, res){
+  res.render("about");
+});
+
+app.listen(3000, function(req, res){
+  console.log("server started on port 3000");
 })
 
-app.listen(3000, function () {
-  console.log('Server started on port 3000.');
-});
